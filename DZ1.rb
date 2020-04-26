8efd3a13654c6c48f33bbe07b78ef91c26cff205
Edward <63458461+BarNeeY-creator@users.noreply.github.com>
2020-04-27 01:11:11 +0300

    first commit

---
+++ DZ1.rb
@@ -0,0 +1,10 @@
+str = ARGV[0]
+#str = gets.chomp
+
+if str =="Titanik"
+ puts  str  +" is a good movie"
+elsif str == "Elki"
+  puts str+"is a bad movie"
+else
+  puts " I haven`t seen " + str +" yet"
+end
